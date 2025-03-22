bffe3db867eee2a2a52e30c43e40a0d4bc7e7366
Shrim01 <shrim01@mail.ru>
2025-03-22 19:58:33 +0500

    c

--- Assets/GamePlay/Script/ControlScript.cs
+++ Assets/GamePlay/Script/ControlScript.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 
-public class ControlScript : MonoBehaviour
+namespace GamePlay.Script
 {
-    void Update()
+    public class ControlScript : MonoBehaviour
     {
-        Rotate();
-    }
+        void Update()
+        {
+            Rotate();
+        }
 
-    private void Rotate()
-    {
-        var position = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
-        var rotation = Mathf.Atan(position.y / position.x) * 180 / Mathf.PI;
-        if (position.x < 0)
-            rotation += 180;
-        transform.rotation = Quaternion.Euler(0, 0, rotation);
+        private void Rotate()
+        {
+            var position = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
+            var rotation = Mathf.Atan(position.y / position.x) * 180 / Mathf.PI;
+            if (position.x < 0)
+                rotation += 180;
+            transform.rotation = Quaternion.Euler(0, 0, rotation);
+        }
     }
 }
