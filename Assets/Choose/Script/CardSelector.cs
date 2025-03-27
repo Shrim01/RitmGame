20ca310af8efeabfd6e3ff85691c44c3c138fee3
Applebombaster <b.danil.a981@gmail.com>
2025-03-28 02:38:12 +0500

    Night 1

--- Assets/Choose/Script/CardSelector.cs
+++ Assets/Choose/Script/CardSelector.cs
@@ -8,6 +8,7 @@ public class CardSelector : MonoBehaviour
     public GameObject cardPrefab; // Префаб карты
     public Transform content; // Контейнер для карт
     public string[] cardNames; // Названия карт
+    public ScrollRect scrollRect; // Ссылка на ScrollRect
 
     void LoadCards()
     {
@@ -56,6 +57,29 @@ public class CardSelector : MonoBehaviour
             entryPointerExit.callback.AddListener((data) => { OnPointerExit(card); });
             eventTrigger.triggers.Add(entryPointerExit);
         }
+
+        // Устанавливаем позицию Scroll View, чтобы первый элемент был в центре
+        CenterScrollView();
+    }
+
+    void CenterScrollView()
+    {
+        if (content.childCount > 0)
+        {
+            // Получаем RectTransform первого элемента
+            RectTransform firstCardRect = content.GetChild(0).GetComponent<RectTransform>();
+
+            // Получаем высоту контента и высоту карточки
+            float contentHeight = content.GetComponent<RectTransform>().rect.height;
+            float cardHeight = firstCardRect.rect.height;
+
+            // Вычисляем целевую позицию для центрирования
+            float targetPosition = (cardHeight / 2) - (contentHeight / 2);
+            float scrollPosition = firstCardRect.anchoredPosition.y + targetPosition;
+
+            // Устанавливаем позицию прокрутки
+            scrollRect.content.anchoredPosition = new Vector2(scrollRect.content.anchoredPosition.x, scrollPosition);
+        }
     }
 
     void OnCardSelected(string cardName)
@@ -65,11 +89,19 @@ public class CardSelector : MonoBehaviour
 
     void OnPointerEnter(GameObject card)
     {
-        card.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f); // Увеличиваем размер
+        // Увеличиваем размер карточки
+        card.transform.localScale = new Vector3(1.7f, 1.7f, 1f);
+
+        // Смещаем карточку влево на 10 единиц (можете изменить это значение по своему усмотрению)
+        card.transform.localPosition += new Vector3(-180f, 0f, 0f);
     }
 
     void OnPointerExit(GameObject card)
     {
-        card.transform.localScale = new Vector3(1f, 1f, 1f); // Возвращаем размер
+        // Возвращаем размер карточки
+        card.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        // Возвращаем карточку на исходную позицию
+        card.transform.localPosition -= new Vector3(-180f, 0f, 0f);
     }
 }
